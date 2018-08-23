8fc58e2fdb783634ccf989c9e2dd9ca7e1e87a85
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2018-08-23 19:22:53 -0300

    Very first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+ruby '2.5.1'
+
+source 'https://rubygems.org/'
+
+gem 'rspec'
+gem 'rubocop'
---
+++ spec/true_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+describe 'hola' do
+  it 'text' do
+    expect(true).to eq(true)
+  end
+end
